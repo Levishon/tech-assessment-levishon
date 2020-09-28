5f86c4789587493c42e15e7c1f246338b2cb1ced
Levishon <lewislevishon@gmail.com>
2020-09-28 15:09:33 +0200

    update

---
+++ Weather/Weather/Controllers/WeatherController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Weather.Models;
+
+namespace Weather.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // GET: Weather
+        public ActionResult GetWeather()
+        {
+            WeatherDBEntities1 db = new WeatherDBEntities1();
+            List<Temp> temps = db.Temps.ToList<Temp>();
+            return View(temps);
+        }
+    }
+}
